5236ca646394e0b8da167534f987a0e23143ffff
chayan0619 <chayankumawat151@gmail.com>
2023-08-14 13:07:28 +0530

    Add project files.

---
+++ WebApplication2-cicid/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication2_cicid.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
